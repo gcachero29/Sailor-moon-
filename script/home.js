5eff48d5620a15210e390d6ae25c33a052d52fea
Gabby Cachero <gcachero29@gmail.com>
2022-08-03 16:31:56 -0500

    commit7

--- script/home.js
+++ script/home.js
@@ -5,7 +5,26 @@ function homepageButton(){
   if(person == null || person == "") {
       text = "User cancelled the prompt";
   } else {
-      text =  "Hello " + person + " Welcome to my page! My name is gabby and this website are about sailor moon characters and my opinion about them. Press a button inside the navbar to explore the page!";
+      text =  "Hello " + person + " Welcome to my page! My name is gabby and this website is about sailor moon characters and my opinion about them. Press a button inside the navbar to explore the page!";
   }
       document.getElementById("text").innerHTML = text;
     }
+
+    mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
