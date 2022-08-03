53727870682c087599ffe99d4740d21942013556
Gabby Cachero <gcachero29@gmail.com>
2022-08-03 11:51:13 -0500

    commit 5

--- script/home.js
+++ script/home.js
@@ -1,11 +1,11 @@
-function myFunction(){
+function homepageButton(){
 
     let text; 
-  let person = prompt (Please enter Name);
+  let person = prompt("Please enter Name");
   if(person == null || person == "") {
       text = "User cancelled the prompt";
   } else {
-      text =  "Hello" + person;
+      text =  "Hello " + person + " Welcome to my page! My name is gabby and this website are about sailor moon characters and my opinion about them. Press a button inside the navbar to explore the page!";
   }
-      document.getElementById("demo").innerHTML = text;
+      document.getElementById("text").innerHTML = text;
     }
